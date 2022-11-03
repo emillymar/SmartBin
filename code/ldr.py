6defd272ceba4adc0de6a74336432e282d66cd86
Emilly <emillygmar.silva@gmail.com>
2022-11-03 18:37:55 -0300

    Using pygame to display image

--- code/ldr.py
+++ code/ldr.py
@@ -1,36 +1,35 @@
-#!/usr/bin/python
 
+#!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
-from PIL import Image
+import pygame
 import subprocess
 import random
 
-ldr_last_state = 1
-
-def open_image(file_name):
-    img_path = f'/home/smartbin/Documents/SmartBin/Pictures/{file_name}.jpeg'
-    img = Image.open(img_path)
-    img.show()
-    return img
+def create_path(file_name):
+	return  f'/home/smartbin/Documents/SmartBin/Pictures/{file_name}.jpeg'
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN)
 
-imgs_default = ['Default1','Default2','Default3']
-imgs_feedback = ['Feedback1','Feedback2','Feedback3']
+imgs_default = ['Default1', 'Default2', 'Default3']
+imgs_feedback = ['Feedback1', 'Feedback2', 'Feedback3']
+
+pygame.init()
+screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+
+def display_image(file_name):
+    screen.fill((0,0,0))
+    img = pygame.image.load(create_path(file_name)).convert()
+    screen.blit(img,(0,0))
+    pygame.display.update()
+
 
 while True:
-    img = open_image(random.choice(imgs_default))
-
-    while True:
-        if GPIO.input(7) == 0:
-            
-            print("Passou o lixo, soltem os memes")
-            subprocess.run(["killall display"], shell=True)
-            img = open_image(random.choice(imgs_feedback))
-            time.sleep(5)
-            subprocess.run(["killall display"], shell=True)
-            img.close()
-            break
-
+	display_image(random.choice(imgs_default))
+	while True:
+		if GPIO.input(7) == 0:
+			print("Passou o lixo, soltem os memes")
+			display_image(random.choice(imgs_feedback))
+			time.sleep(5)
+			break
---
+++ code/start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+python telegram-smartbin.py &
+python teste2.py &
